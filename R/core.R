b55cd18f9f4c1d82a1d9234e69f98d7e0808bcae
Nuno Fachada <faken@fakenmc.com>
2022-01-28 23:51:08 +0000

    Add MathJax support for docs

--- R/core.R
+++ R/core.R
@@ -4,12 +4,15 @@
 #' Determine coordinates of points on a line with `center` and `direction`,
 #' based on the distances from the center given in `dist_center`.
 #'
-#' @param center Center of the line (\eqn{n}-component vector).
-#' @param direction Line direction (\eqn{n}-component unit vector).
-#' @param dist_center  Distance of each point to the center of the line
-#' (\eqn{n}-component vector, where \eqn{n} is the number of points).
+#' \loadmathjax
 #'
-#' @return Coordinates of points on the specified line (\eqn{p \times n} matrix).
+#' @param center Center of the line (\mjeqn{n}{n}-component vector).
+#' @param direction Line direction (\mjeqn{n}{n}-component unit vector).
+#' @param dist_center  Distance of each point to the center of the line
+#' (\eqn{n}-component vector, where \mjeqn{n}{n} is the number of points).
+#'
+#' @return Coordinates of points on the specified line
+#' (\mjeqn{p \times n}{p x n} matrix).
 #'
 #' @export
 points_on_line <- function(center, direction, dist_center) {
