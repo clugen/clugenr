1eb3fac597c6ad2fd5398254d738cd843d3c9a41
Nuno Fachada <faken@fakenmc.com>
2022-01-30 20:44:54 +0000

    Refactor points_on_line() tests to not to use too many for loops

--- tests/testthat/test_01_core.R
+++ tests/testthat/test_01_core.R
@@ -1,46 +1,67 @@
 # Copyright (c) 2020-2022 Nuno Fachada
 # Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
+# Number of line directions to test
 ndirs <- 2
+
+# Number of line centers to test
 ncts <- 2
 
-for (seed in seeds) {
-set.seed(seed)
-  for (nd in num_dims) {
-    for (len in llengths_mus) {
-      for (tpts in num_points[num_points < 1000]) {
-        for (direc in asplit(get_unitvecs(ndirs, nd), 1)) {
-          for (ctr in asplit(get_vecs(ndirs, nd), 1)) {
-            test_desc <- paste0("points_on_line: seed=", seed, ", nd=", nd,
-                                ", len=", len, ", tpts=", tpts,
-                                ", direc=[", paste(direc, collapse = ", "),
-                                "], ctr=[", paste(ctr, collapse = ", "), "]")
+# Create parameter combinations to test
+targs <- expand.grid(seed = seeds, nd = num_dims, len = llengths_mus,
+                     tpts = num_points[num_points < 1000])
 
-            test_that(test_desc, {
+# Loop through all parameter combinations
+for (i in seq.int(1, nrow(targs))) {
 
-              # Create some random distances from center
-              dist2ctr <- len * runif(tpts) - len / 2
+  # Get current parameters
+  seed <- targs[i, "seed"]
+  tpts <- targs[i, "tpts"]
+  nd <- targs[i, "nd"]
+  len <- targs[i, "len"]
 
-              # Check that the points_on_line function runs without warnings
-              expect_warning(pts <- points_on_line(ctr, direc, dist2ctr),
-                             regexp = NA)
+  # Set seed
+  set.seed(seed)
 
-              # Check that the dimensions agree
-              expect_equal(dim(pts), c(tpts, nd))
+  # Create combination of seed-depending parameters (line directions + centers)
+  tsargs <- expand.grid(direc = asplit(get_unitvecs(ndirs, nd), 1),
+                        ctr = asplit(get_vecs(ncts, nd), 1))
 
-              # Check that distance of points to the line is approximately zero
-              for (pt in asplit(pts, 1)) {
-                # Get distance from current point to line
-                ptv <- as.vector(pt)
-                dp2l <- (ptv - ctr) - c((ptv - ctr) %*% direc) * direc
-                d <- norm(as.matrix(dp2l, type = "2"))
-                # Check that it is approximately zero
-                expect_equal(d, 0)
-              }
-            })
-          }
-        }
+  # Loop through line directions and line centers
+  for (j in seq.int(1, nrow(tsargs))) {
+
+    # Get current line direction + line center
+    direc <- tsargs[j, "direc"][[1]]
+    ctr <- tsargs[j, "ctr"][[1]]
+
+    # Determine test name for current parameter set
+    test_desc <- paste0("points_on_line: nd=", nd,
+                        ", len=", len, ", tpts=", tpts,
+                        ", direc=[", paste(direc, collapse = ", "),
+                        "], ctr=[", paste(ctr, collapse = ", "), "]")
+
+    # Perform tests for current parameter set
+    test_that(test_desc, {
+
+      # Create some random distances from center
+      dist2ctr <- len * runif(tpts) - len / 2
+
+      # Check that the points_on_line function runs without warnings
+      expect_warning(pts <- points_on_line(ctr, direc, dist2ctr),
+                     regexp = NA)
+
+      # Check that the dimensions agree
+      expect_equal(dim(pts), c(tpts, nd))
+
+      # Check that distance of points to the line is approximately zero
+      for (pt in asplit(pts, 1)) {
+        # Get distance from current point to line
+        ptv <- as.vector(pt)
+        dp2l <- (ptv - ctr) - c((ptv - ctr) %*% direc) * direc
+        d <- norm(as.matrix(dp2l, type = "2"))
+        # Check that it is approximately zero
+        expect_equal(d, 0)
       }
-    }
+    })
   }
 }
--- tests/testthat/test_05_lintr.R
+++ tests/testthat/test_05_lintr.R
@@ -2,6 +2,5 @@
 # Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
 test_that("Package has no lintr errors", {
-  lintr::expect_lint_free(
-    linters = lintr::with_defaults(cyclocomp_linter = NULL))
-  })
+  lintr::expect_lint_free()
+})
