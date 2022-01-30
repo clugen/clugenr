b600fda230bb0fba3f3c4661cf220c62ca08a790
Nuno Fachada <faken@fakenmc.com>
2022-01-30 00:45:52 +0000

    Add lintr and points_on_line() [WIP] unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,7 @@
+# Copyright (c) 2020-2022 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+library(clugenr)
+library(testthat)
+
+test_check("clugenr")
---
+++ tests/testthat/test_01_lintr.R
@@ -0,0 +1,6 @@
+# Copyright (c) 2020-2022 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+test_that("Package has no lintr errors", {
+  lintr::expect_lint_free()
+})
