d5c67dd1d482813ae0dd27b3f5bf0cf323191a39
Nuno Fachada <faken@fakenmc.com>
2022-02-02 17:47:51 +0000

    Add workaround for bug in lintr which stops check using RStudio

--- tests/testthat/test_05_lintr.R
+++ tests/testthat/test_05_lintr.R
@@ -1,6 +1,28 @@
 # Copyright (c) 2020-2022 Nuno Fachada
 # Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
-test_that("Package has no lintr errors", {
-  lintr::expect_lint_free()
-})
+# Workaround due to bug in lintr: https://github.com/r-lib/lintr/issues/421
+if (requireNamespace("lintr", quietly = TRUE)) {
+  library(lintr)
+
+  test_that("Package has no lintr errors", {
+
+    # A duplicate copy of the find_package function from lintr
+    find_package <- function(path) {
+      path <- normalizePath(path, mustWork = FALSE)
+
+      while (!file.exists(file.path(path, "DESCRIPTION"))) {
+        path <- dirname(path)
+        if (identical(path, dirname(path))) {
+          return(NULL)
+        }
+      }
+
+      path
+    }
+
+    if (!is.null(find_package("."))) {
+      expect_lint_free()
+    }
+  })
+}
