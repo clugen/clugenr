338e7e1330ebae10ce5f6b48e8e9640fe3914b18
Nuno Fachada <faken@fakenmc.com>
2022-02-03 20:13:45 +0000

    Add templates for remaining functions and tests

--- R/module.R
+++ R/module.R
@@ -1,7 +1,58 @@
 # Copyright (c) 2020-2022 Nuno Fachada
 # Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
-#' Determine cluster sizes.
+#' Todo this text.
+#'
+#' @param num_clusters TODO.
+#' @param angle_disp TODO.
+#' @return TODO.
+#'
+#' @export
+angle_deltas <- function(num_clusters, angle_disp) {
+
+}
+
+#' Todo this text.
+#'
+#' @param num_clusters TODO.
+#' @param clu_sep TODO.
+#' @param clu_offset TODO.
+#' @return TODO.
+#'
+#' @export
+clucenters <- function(num_clusters, clu_sep, clu_offset) {
+
+}
+
+#' Todo this text.
+#'
+#' @param projs TODO.
+#' @param lat_disp TODO.
+#' @param line_len TODO.
+#' @param clu_dir TODO.
+#' @param clu_ctr TODO.
+#' @return TODO.
+#'
+#' @export
+clupoints_n_1 <- function(projs, lat_disp, line_len, clu_dir, clu_ctr) {
+
+}
+
+#' Todo this text.
+#'
+#' @param projs TODO.
+#' @param lat_disp TODO.
+#' @param line_len TODO.
+#' @param clu_dir TODO.
+#' @param clu_ctr TODO.
+#' @return TODO.
+#'
+#' @export
+clupoints_n <- function(projs, lat_disp, line_len, clu_dir, clu_ctr) {
+
+}
+
+#' Todo this text.
 #'
 #' @param num_clusters TODO.
 #' @param num_points TODO.
@@ -15,14 +66,12 @@ clusizes <- function(num_clusters, num_points, allow_empty) {
 
 #' Todo this text.
 #'
-#' @param projs TODO.
-#' @param lat_disp TODO.
-#' @param line_len TODO.
-#' @param clu_dir TODO.
-#' @param clu_ctr TODO.
-#' @return TODO. TODO.
+#' @param num_clusters TODO.
+#' @param llength TODO.
+#' @param llength_disp TODO.
+#' @return TODO.
 #'
 #' @export
-clupoints_n_1 <- function(projs, lat_disp, line_len, clu_dir, clu_ctr) {
+llengths <- function(num_clusters, llength, llength_disp) {
 
 }
---
+++ tests/testthat/test_12_clusizes.R
@@ -0,0 +1,3 @@
+# Copyright (c) 2020-2022 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
