9848d1259975505d5322d8ff15d073f4f659daa9
Nuno Fachada <faken@fakenmc.com>
2022-02-03 15:06:46 +0000

    Implement fix_empty()

--- R/helper.R
+++ R/helper.R
@@ -23,6 +23,37 @@
 #' (vector of size \mjseqn{c}).
 #'
 #' @export
-fix_empty <- function(clu_num_points, allow_empty) {
+#'
+#' @examples
+#'
+#' clusters <- c(3, 4, 5, 0, 0)    # A vector with some empty elements
+#' clusters <- fix_empty(clusters) # Apply this function
+#' clusters                        # Check that there's no more empty elements
+#' # [1] 3 3 4 1 1
+fix_empty <- function(clu_num_points, allow_empty = FALSE) {
 
+  # If the allow_empty parameter is set to true, don't change the number of
+  # points in each cluster; this is useful for quick `clusizes_fn` one-liners
+  if (!allow_empty) {
+
+    # Find empty clusters
+    empty_clusts <- which(clu_num_points == 0)
+
+    # If there are empty clusters and enough points for all clusters...
+    if (length(empty_clusts) > 0 &&
+        sum(clu_num_points) >= length(clu_num_points)) {
+
+      # Go through the empty clusters...
+      for (i0 in empty_clusts) {
+
+        # ...get a point from the largest cluster and assign it to the
+        # current empty cluster
+        imax <- which.max(clu_num_points)
+        clu_num_points[imax] <- clu_num_points[imax] - 1
+        clu_num_points[i0] <- 1
+      }
+    }
+  }
+
+  clu_num_points
 }
