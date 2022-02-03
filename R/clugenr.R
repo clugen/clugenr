a95d49aebe4b61f893b4aacb6a75578ff21747ab
Nuno Fachada <faken@fakenmc.com>
2022-02-03 11:44:07 +0000

    Add package-level docs

---
+++ R/clugenr.R
@@ -0,0 +1,17 @@
+# Copyright (c) 2020-2022 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#' clugenr: Multidimensional cluster generation in R
+#'
+#' clugenr is an R package for generating multidimensional clusters. Each
+#' cluster is supported by a line segment, the position, orientation and length
+#' of which guide where the respective points are placed.
+#'
+#' @author Nuno Fachada
+#'
+#' @note MIT License
+#'
+#' @docType package
+#' @name clugenr
+NULL
+#> NULL
