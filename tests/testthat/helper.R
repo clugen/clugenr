7b18f0e320291ae1a66d9bfd0caf0f9b86c034ff
Nuno Fachada <faken@fakenmc.com>
2022-02-03 00:35:24 +0000

    Add tests for rand_vector_at_angle()

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -14,3 +14,38 @@ get_unitvecs <- function(n, nd) {
   }
   m
 }
+
+# Get n angles
+get_angles <- function(n) {
+  2 * pi * stats::runif(n) - pi
+}
+
+# Get angle between two vectors, useful for checking correctness of results
+# Based on AngleBetweenVectors.jl by Jeffrey Sarnoff (MIT license),
+# https://github.com/JeffreySarnoff/AngleBetweenVectors.jl
+# in turn based on these notes by Prof. W. Kahan, see page 15:
+# https://people.eecs.berkeley.edu/~wkahan/MathH110/Cross.pdf
+angle_btw <- function(v1, v2) {
+
+  signbit <- function(x) {
+    x < 0
+  }
+
+  u1 <- v1 / norm(v1, "2")
+  u2 <- v2 / norm(v2, "2")
+
+  y <- u1 - u2
+  x <- u1 + u2
+
+  a0 <- 2 * atan(norm(y, "2") / norm(x, "2"))
+
+  if (!(signbit(a0) || signbit(pi - a0))) {
+    a <- a0
+  } else if (signbit(a0)) {
+    a <- 0.0
+  } else {
+    a <- pi
+  }
+
+  a
+}
---
+++ tests/testthat/test_04_rand_vector_at_angle.R
@@ -0,0 +1,56 @@
+# Copyright (c) 2020-2022 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+# How many vectors to test?
+nvec <- 5
+
+# How many angles to test?
+nang <- 5
+
+# Create parameter combinations to test
+targs <- expand.grid(seed = seeds, nd = num_dims)
+
+# Loop through all parameter combinations
+for (i in seq.int(1, nrow(targs))) {
+
+  # Get current parameters
+  seed <- targs[i, "seed"]
+  nd <- targs[i, "nd"]
+
+  # Set seed
+  set.seed(seed)
+
+  # Create combination of seed-depending parameters (line directions + centers)
+  tsargs <- expand.grid(u = asplit(get_unitvecs(nvec, nd), 1),
+                        a = get_angles(nang))
+
+  # Loop through line directions and line centers
+  for (j in seq.int(1, nrow(tsargs))) {
+
+    # Get current vector and angle
+    u <- tsargs[j, "u"][[1]]
+    a <- tsargs[j, "a"]
+
+    # Determine test name for current parameter set
+    test_desc <- paste0("rand_vector_at_angle: nd=", nd, ", a=", a,
+                        ", u=[", paste(u, collapse = ", "), "]")
+
+    # Perform tests for current parameter set
+    test_that(test_desc, {
+
+      # Check that the function runs without warnings
+      expect_warning(r <- rand_vector_at_angle(u, a), regexp = NA)
+
+      # Check that returned vector has the correct length
+      expect_equal(length(r), nd)
+
+      # Check that returned vector has norm == 1
+      expect_equal(norm(r, "2"), 1)
+
+      # Check that vectors u and r have an angle of a between them
+      if (nd > 1 && abs(a) < pi / 2) {
+        expect(angle_btw(u, r), abs(a))
+      }
+    })
+  }
+}
