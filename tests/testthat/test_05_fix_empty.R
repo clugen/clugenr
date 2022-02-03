6db329976f79823cafee7d70bdc1fc28ecc81329
Nuno Fachada <faken@fakenmc.com>
2022-02-03 16:29:21 +0000

    Add tests for fix_empty()

---
+++ tests/testthat/test_05_fix_empty.R
@@ -0,0 +1,48 @@
+# Copyright (c) 2020-2022 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+test_that("fix_empty works as expected", {
+
+  #  No empty clusters
+  clusts <- c(11, 21, 10)
+  expect_warning(clusts_fixed <- fix_empty(clusts, allow_empty = FALSE),
+                 regexp = NA)
+  expect_equal(clusts, clusts_fixed)
+
+  # Empty clusters, no fix
+  clusts <- c(0, 11, 21, 10, 0, 0)
+  expect_warning(clusts_fixed <- fix_empty(clusts, allow_empty = TRUE),
+                 regexp = NA)
+  expect_equal(clusts, clusts_fixed)
+
+  # Empty clusters, fix
+  clusts <- c(5, 0, 21, 10, 0, 0, 101)
+  expect_warning(clusts_fixed <- fix_empty(clusts), regexp = NA)
+  expect_equal(sum(clusts), sum(clusts_fixed));
+  expect_false(isTRUE(all.equal(clusts, clusts_fixed)))
+  expect_false(any(clusts_fixed == 0));
+
+  # Empty clusters, fix, several equal maximums
+  clusts = c(101, 5, 0, 21, 101, 10, 0, 0, 101, 100, 99, 0, 0, 0, 100)
+  expect_warning(clusts_fixed <- fix_empty(clusts), regexp = NA)
+  expect_equal(sum(clusts), sum(clusts_fixed));
+  expect_false(isTRUE(all.equal(clusts, clusts_fixed)))
+  expect_false(any(clusts_fixed == 0));
+
+  # Empty clusters, no fix (flag)
+  clusts = c(0, 10)
+  expect_warning(clusts_fixed <- fix_empty(clusts, allow_empty = TRUE),
+                 regexp = NA)
+  expect_equal(clusts, clusts_fixed);
+
+  # Empty clusters, no fix (not enough points)
+  clusts = c(0, 1, 1, 0, 0, 2, 0, 0)
+  expect_warning(clusts_fixed <- fix_empty(clusts), regexp = NA)
+  expect_equal(clusts, clusts_fixed);
+
+  # Works with 1D
+  clusts = 100;
+  expect_warning(clusts_fixed <- fix_empty(clusts), regexp = NA)
+  expect_equal(clusts, clusts_fixed);
+
+})
