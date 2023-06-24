b61a3e82aa337f661f33d76070eb4166069aa5a2
Nuno Fachada <faken@fakenmc.com>
2023-06-24 20:41:55 +0100

    Make plot examples 2D be consistent for specific levels

--- vignettes/plot_examples_2d.R
+++ vignettes/plot_examples_2d.R
@@ -7,14 +7,18 @@ library(patchwork) # For combining 2D plots
 # Possible alternative color scales if we want consistency between subplots with
 # different number of clusters
 color_scales = list(
-  seaborn = c("#1F77B4", "#FF7F0E", "#2CA02C", "#D62728", "#9467BD",
-              "#8C564B", "#E377C2", "#7F7F7F", "#BCBD22", "#17BECF"),
-  set2 = c("#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854",
-           "#FFD92F", "#E5C494", "#B3B3B3", "#A1C9F4", "#4FB031"),
-  pastel = c("#A1C9F4", "#FFB482", "#8DE5A1", "#FF9F9B", "#D0BBFF",
-             "#DEBB9B", "#FAB0E4", "#CFCFCF", "#FFFEA3", "#B9F2F0"),
-  husl9 = c("#F67088", "#D58C31", "#A39F31", "#4FB031", "#34AE90",
-            "#36AAB5", "#3BA3EC", "#BA82F4", "#F563D3", "#FF7F0E"))
+  seaborn = c("1" = "#1F77B4", "2" = "#FF7F0E", "3" = "#2CA02C", "4" = "#D62728",
+              "5" = "#9467BD", "6" = "#8C564B", "7" = "#E377C2", "8" = "#7F7F7F",
+              "9" = "#BCBD22", "10" = "#17BECF"),
+  set2 = c("1" = "#66C2A5", "2" = "#FC8D62", "3" = "#8DA0CB", "4" = "#E78AC3",
+           "5" = "#A6D854", "6" = "#FFD92F", "7" = "#E5C494", "8" = "#B3B3B3",
+           "9" = "#A1C9F4", "10" = "#4FB031"),
+  pastel = c("1" = "#A1C9F4", "2" = "#FFB482", "3" = "#8DE5A1", "4" = "#FF9F9B",
+             "5" = "#D0BBFF", "6" = "#DEBB9B", "7" = "#FAB0E4", "8" = "#CFCFCF",
+             "9" = "#FFFEA3", "10" = "#B9F2F0"),
+  husl9 = c("1" = "#F67088", "2" = "#D58C31", "3" = "#A39F31", "4" = "#4FB031",
+            "5" = "#34AE90", "6" = "#36AAB5", "7" = "#3BA3EC", "8" = "#BA82F4",
+            "9" = "#F563D3", "10" = "#FF7F0E"))
 
 # Function for plotting a series of related 2D examples
 plot_examples_2d <- function(..., pmargin = 0.1, palette = NULL) {
@@ -27,8 +31,6 @@ plot_examples_2d <- function(..., pmargin = 0.1, palette = NULL) {
     NULL
   } else {
     scale_fill_manual(values = color_scales[[palette]])
-    # Gather all levels and apply them to the input data
-    all_lvls <- unlist(lapply(ets, function(et) unique(et$e$clusters)))
   }
 
   # Get maximum and minimum points in each dimension for all examples
@@ -55,7 +57,6 @@ plot_examples_2d <- function(..., pmargin = 0.1, palette = NULL) {
     ets,
     function(et) {
       e <- et$e
-      levels(e$clusters) <- all_lvls
       t <- et$t
       ggplot(NULL, aes(x = e$points[, 1], y = e$points[, 2])) +
         geom_point(shape = 21, colour = "white", stroke = 0.1, alpha = 0.8,
