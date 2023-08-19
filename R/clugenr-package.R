cdc5c4b1886f08dde596ca18dfe3faef1d9d2012
Nuno Fachada <faken@fakenmc.com>
2023-08-19 19:36:37 +0100

    Fix packaging issue due to roxygen update

---
+++ R/clugenr-package.R
@@ -0,0 +1,10 @@
+# Copyright (c) 2020-2023 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom mathjaxr preview_rd
+## usethis namespace: end
+NULL
